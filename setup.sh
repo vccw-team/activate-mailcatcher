b54976a99ff7e48bd4266e14de51e3ec12619a46
miya0001 <miya@wpist.me>
2015-08-09 12:46:58 +0900

    initial release

--- setup.sh
+++ setup.sh
@@ -2,4 +2,23 @@
 
 set -ex
 
-mkdir -p `wp eval 'echo WP_CONTENT_DIR;'`/mu-plugins
+WP_MU_PUGINS_DIR=`wp eval 'echo WP_CONTENT_DIR;'`/mu-plugins
+VCCW_ADDRESS=`ohai | jq -r .network.interfaces.eth1.routes[0].src`
+
+mkdir -p $WP_MU_PUGINS_DIR
+
+echo "{
+    \"name\": \"mu-plugins\",
+    \"require\": {
+        \"vccw/mailcatcher\": \"*\"
+    }
+}" > ${WP_MU_PUGINS_DIR}/composer.json
+
+echo "<?php
+/*
+Plugin Name: VCCW
+*/
+
+require_once dirname( __FILE__ ) . '/vendor/autoload.php';" > ${WP_MU_PUGINS_DIR}/plugin.php
+
+mailcatcher --http-ip=${VCCW_ADDRESS}
