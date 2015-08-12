ba2613f3b753893290fecd4ad9501002ef082d31
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2015-08-12 16:52:16 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,11 +4,16 @@ set -ex
 
 WP_MU_PUGINS_DIR=$(wp eval 'echo WP_CONTENT_DIR;')/mu-plugins
 
+if [ -e ${WP_MU_PUGINS_DIR}/composer.json ] ; then
+    echo ${WP_MU_PUGINS_DIR}'/composer.json already exists.'
+    exit 1
+fi
+
 mkdir -p $WP_MU_PUGINS_DIR
 
 echo "{
     \"name\": \"mu-plugins\",
-    \"require\": {
+    \"require-dev\": {
         \"vccw/mailcatcher\": \"*\"
     }
 }" > ${WP_MU_PUGINS_DIR}/composer.json
@@ -26,3 +31,5 @@ Description: Re-routes all WordPress emails to MailCatcher.
 require_once dirname( __FILE__ ) . '/vendor/autoload.php';" > ${WP_MU_PUGINS_DIR}/mailcatcher.php
 
 mailcatcher --http-ip=$(ohai | jq -r .network.interfaces.eth1.routes[0].src)
+
+exit 0
